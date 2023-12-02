c001214aef55121ad328f99a7c27145d56915d10
bellieaziza <azizatulkinova2004@gmail.com>
2023-12-02 22:17:41 +0500

    Added task21.dart file

---
+++ Homework.dart/task21.dart
@@ -0,0 +1,48 @@
+// Future<void> main() async{
+//   Stream<int> .periodic(
+//     Duration(seconds: 1),
+//     (value)=> value,
+//   ) .take(10).listen((event)=> print(event));
+// }
+
+
+
+
+// Future<void> main() async{
+//   Stream<int> .periodic(
+//     Duration(seconds: 0),
+//     (value)=> value,
+//   ) .take(10).listen((event)=> print(event));
+// }
+
+
+
+
+
+// Future<void> main() async{
+//   Stream<int> .periodic(
+//     Duration(seconds: 1),
+//     (value)=> value,
+//   ).take(10).listen((event) => print(event));
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
